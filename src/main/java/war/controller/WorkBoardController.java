e2529f59ed54ebf4ac3234fb7d341b587a48aff8
Venki Balasubramanian <venki.balasubramanian@rmit.edu.au>
2012-10-23 05:30:50 +0000

    Complete the CRUD for Workboard
    git-svn-id: file:///Users/ravisreenivas/GitConversions/local-svn/SEAPORTS/trunk/CSS@481 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/main/java/war/controller/WorkBoardController.java
+++ src/main/java/war/controller/WorkBoardController.java
@@ -1,9 +1,5 @@
 package war.controller;
 
-
-
-
-
 import war.dao.*;
 import war.model.* ;
 
@@ -32,32 +28,26 @@ public class WorkBoardController {
 	@Autowired
 	private PersonDao personDao ;
 	private Person person ;
+
 	
-/*	
- * @RequestMapping(method=RequestMethod.GET,value="edit")
-	public ModelAndView editPerson(@RequestParam(value="id",required=false) Long id) {		
-		logger.debug("Received request to edit person id : "+id);				
-		ModelAndView mav = new ModelAndView();		
- 		mav.setViewName("edit");
- 		Person person = null;
- 		if (id == null) {
- 			person = new Person();
- 		} else {
- 			person = personDao.find(id);
- 		}	
- 		mav.addObject("person", person);
-		return mav;	
+
+	@ModelAttribute("workboard")
+	@RequestMapping(value = "/uploadfile", method = RequestMethod.GET)
+	public String Uploadfile(Model model) {
+		logger.info("Inside File upload !");
+		model.addAttribute("controllerMessage", "Please Login to Create Workboard");
+		return "uploadfile";
 	}
-	*/
 	
-//	@ModelAttribute("workboard")
-	@RequestMapping(method = RequestMethod.GET)
+	
+	@ModelAttribute("workboard")
+	@RequestMapping(value ="/add",method = RequestMethod.GET)
 	public ModelAndView CreateWorkBoard(@RequestParam(value="firstName",required=true) String firstName, Model model) {
+		System.out.println("Inside CreateWorkBoard" + firstName) ;
 		person = personDao.find(firstName) ;
 		ModelAndView mav = new ModelAndView();
 		WorkBoard workboard = new WorkBoard();
 		workboard.setPerson(person) ;
-		System.out.println(workboard.getWorkBoardID()) ;
 		mav.setViewName("createwb");
  		mav.addObject("workboard", workboard);
 		model.addAttribute("firstName",person.getFirstName()) ;
@@ -65,21 +55,47 @@ public class WorkBoardController {
 		return mav;
 	}
 	
-	@RequestMapping(method=RequestMethod.POST) 
-	public ModelAndView saveWorkboard(@ModelAttribute WorkBoard workboard) {
+	@RequestMapping(value = "/add",method=RequestMethod.POST) 
+	public ModelAndView saveWorkboard(@ModelAttribute WorkBoard workboard,@RequestParam(value="firstName",required=true) String firstname, Model model) {
 		logger.debug("Received object for workboard  "+ workboard);
-		System.out.println(workboard.getPerson()) ;
-//		person = personDao.find(firstName);
+		String username = person.getFirstName() ;
 		workboard.setPerson(person) ;
-		
+		System.out.println("Inside saveWorkboard 1 : " + workboard.getPerson() + username) ;	
+		System.out.println("Inside saveWorkboard 2 : " + workboard.getWorkBoardName() ) ;
 		workboardDao.save(workboard);
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("createdWB") ;
+		ModelAndView mav = new ModelAndView();	
+		System.out.println("Inside saveWorkboard 3 : " + workboard.getWorkBoardID() ) ;
+		model.addAttribute("workboardid",workboard.getWorkBoardID() );
 		mav.addObject("workboard", workboard);
+		mav.setViewName("createdWB") ;
 		return mav ;
 		
 	}
 	
+
+	@RequestMapping(value = "/delete",method=RequestMethod.GET) 
+	public String deleteWorkboard(@ModelAttribute WorkBoard workboard,@RequestParam(value="workboardid",required=true) Integer Id, Model model) {
+		logger.debug("Received object for workboard  "+ workboard);
+		System.out.println("Inside saveWorkboard 4 Id : " + Id ) ;
+		workboard = workboardDao.find(Id) ;
+		System.out.println("Inside saveWorkboard 5 workboard : " + workboard ) ;
+		person = workboard.getPerson() ;
+		System.out.println("Inside saveWorkboard 8.1 workboard : " + Id ) ;
+		workboardDao.removeWorkBoard(Id) ;
+		System.out.println("Inside saveWorkboard 9 workboard : " + Id ) ;
+		ModelAndView mav = new ModelAndView();
+		WorkBoard wb = new WorkBoard();
+		System.out.println("Inside saveWorkboard 10 workboard : " + wb ) ;
+		wb.setPerson(person) ;
+//		mav.setViewName("createwb");
+ 		mav.addObject("workboard", wb);
+		model.addAttribute("firstName",person.getFirstName()) ;
+// 		model.addAttribute("secondName",person.getLastName()) ;
+		return "redirect:add" ;
+//		return mav;		
+	}
+	
+
 /*	
  * @RequestMapping(method=RequestMethod.GET,value="list")
 	public ModelAndView showPeopleView(@ModelAttribute Person person) {
