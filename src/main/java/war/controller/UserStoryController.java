b2e118ae09cd0d9b2722e5d5068827c4a3b85bfd
Venki Balasubramanian <venki.balasubramanian@rmit.edu.au>
2012-12-14 05:47:15 +0000

    The UserStory controller for User Story CRUD operation
    git-svn-id: file:///Users/ravisreenivas/GitConversions/local-svn/SEAPORTS/trunk/CSS@536 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ src/main/java/war/controller/UserStoryController.java
@@ -0,0 +1,130 @@
+package war.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import war.dao.*;
+import war.model.* ;
+import war.service.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import org.springframework.ui.Model;
+
+import org.apache.commons.codec.binary.* ;
+import org.springframework.web.servlet.tags.* ; 
+import org.apache.commons.lang.* ; 
+
+
+@Controller
+@RequestMapping("/createus")
+public class UserStoryController {
+		
+	private static final Logger logger = LoggerFactory.getLogger(PersonController.class);
+	@Autowired
+	private FilesDao filesDao ;
+	@Autowired
+	private WorkBoardDao workboardDao;
+	@Autowired
+	private PersonDao personDao ;
+	private Person person ;
+	private WorkBoard workboard ;
+	
+	private WorkBoardController cworkboard ;
+	private Files file ;
+	private String splitvar = "\\." ;
+	private String [] tokens;
+
+	@ModelAttribute("workboard")
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView getUserWorkBoard(@RequestParam(value="workboardid",required=true) Integer Id, Model model) {
+		logger.info("Inside get Work Board !");
+		
+		String firstName ;
+		cworkboard = new WorkBoardController() ;
+		workboard = workboardDao.find(Id) ;
+		person = personDao.find(workboard.getPerson().getFirstName()) ;
+		firstName = person.getFirstName() ;
+		workboard = workboardDao.getActiveWorkBoard(person) ; 
+		if (workboard == null){
+			ModelAndView mav = cworkboard.CreateWorkBoard(firstName,model) ;
+			return mav;	
+		} else {	
+			workboard = workboardDao.find(Id) ; 
+		}
+		workboard.setMode("passive") ;
+		workboardDao.save(workboard) ;
+		
+		ModelAndView mav = modelForPassiveWBview(model, workboard);
+		return mav;	
+	}
+	
+	private ModelAndView modelForPassiveWBview(Model model, WorkBoard workboard) {
+		FilesService stringfiles;
+		Files swapfile;
+		
+		System.out.println("Inside the refractor" + person +" "+ personDao + " " + workboard);
+		
+		
+		/* ** CRUD operation for the file and to redirect activeWB.jsp View** */
+		ModelAndView mav = new ModelAndView();
+		person = personDao.find(workboard.getPerson().getFirstName()) ;
+		
+ 		
+ 		List<Files> convertedfiles = new ArrayList<Files>() ;
+ 		
+ 		try {
+			/////// Converting the bytefiles to stringfiles     ///////
+			List<Files> files = filesDao.getFiles(workboard);
+			Files stringfile;
+			for (int i = 0, n = files.size(); i < n; i++) {
+				stringfile = new Files();
+				swapfile = new Files();
+				swapfile = files.get(i);
+				stringfile.setFileid(swapfile.getFileid());
+				stringfile.setFilename(swapfile.getFilename());
+				stringfile.setType(swapfile.getType());
+				stringfile.setWorkboard(swapfile.getWorkboard());
+				stringfile.setFile(swapfile.getFile());
+				if (swapfile.getType() == "jpg" || swapfile.getType() == "jpeg" ) {
+					System.out.println("Inside upload " + swapfile.getFile()) ;
+					stringfile.setFilecontent(Base64.encodeBase64String(swapfile.getFile())) ; 
+				}else {
+					stringfile.setFilecontent(swapfile.toString((swapfile.getFile())));
+				}
+				convertedfiles.add(stringfile);
+			}
+		} catch (NullPointerException e) {
+				convertedfiles.add(null) ;
+		}
+		stringfiles = new FilesService() ;
+ 		stringfiles.setFiles(convertedfiles) ;
+ 		
+ 		mav.addObject("workboard", workboard) ;
+ 		file = new Files() ;
+ 		mav.addObject(file) ;  // This file object id for the userwbmenu.jsp
+		model.addAttribute("firstName",person.getFirstName()) ;
+ 		model.addAttribute("secondName",person.getLastName()) ;	
+ 		model.addAttribute("workboardTitle", workboard.getWorkBoardName()) ;
+ 		model.addAttribute("workboardID", workboard.getWorkBoardID());
+ 		mav.addObject("stringfiles",stringfiles) ;
+		mav.setViewName("passiveWB");
+		return mav;
+	}
+	
+	
+	
+	
+	
+}
