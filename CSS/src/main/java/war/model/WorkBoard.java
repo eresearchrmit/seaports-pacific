ca97394edd650f8865a1885135fc7888697a634c
Venki Balasubramanian <venki.balasubramanian@rmit.edu.au>
2012-12-21 00:35:28 +0000

    Updated Workboard
    git-svn-id: file:///Users/ravisreenivas/GitConversions/local-svn/SEAPORTS/trunk@543 e6edf6fb-f266-4316-afb4-e53d95876a76

--- CSS/src/main/java/war/model/WorkBoard.java
+++ CSS/src/main/java/war/model/WorkBoard.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 import org.hibernate.annotations.LazyCollection;
 import org.hibernate.annotations.LazyCollectionOption;
@@ -27,6 +29,7 @@ public class WorkBoard {
 	private String mode ;
 	
 	private List<Files> files ;
+	private UserStory userstory ;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
@@ -54,6 +57,15 @@ public class WorkBoard {
 
 	public void setPerson(Person person) {
 		this.person = person;
+	}
+	
+	@OneToOne(targetEntity=UserStory.class, mappedBy="workboard", cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+	public UserStory getUserstory() {
+		return userstory;
+	}
+
+	public void setUserstory(UserStory userstory) {
+		this.userstory = userstory;
 	}
 
 	public static long getSerialversionuid() {
