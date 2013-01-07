4e14e4ba096ef31caec3d6dafa69fbd2469231d0
Venki Balasubramanian <venki.balasubramanian@rmit.edu.au>
2013-01-07 04:58:15 +0000

    Included the Dataelement service for user story creation
    git-svn-id: file:///Users/ravisreenivas/GitConversions/local-svn/SEAPORTS/trunk@559 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ CSS/src/main/java/war/service/DataElementService.java
@@ -0,0 +1,19 @@
+package war.service;
+
+import java.util.List;
+
+import war.model.DataElement;
+
+public class DataElementService {
+
+	private List<DataElement> dataelements ;
+
+	public List<DataElement> getDataElements() {	
+		return dataelements;
+	}
+
+	public void setDataElements(List<DataElement> dataelements) { 
+		this.dataelements = dataelements ;	
+	}
+	
+}
