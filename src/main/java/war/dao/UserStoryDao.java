bc3ef68d3cbbfb266de1b7d0717d6832481ef198
Venki Balasubramanian <venki.balasubramanian@rmit.edu.au>
2013-01-07 04:55:06 +0000

    Added the UserStory Dao for the User Story creation.
    git-svn-id: file:///Users/ravisreenivas/GitConversions/local-svn/SEAPORTS/trunk/CSS@557 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ src/main/java/war/dao/UserStoryDao.java
@@ -0,0 +1,70 @@
+package war.dao;
+import war.model.Person;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import war.model.UserStory;
+import war.model.WorkBoard;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class UserStoryDao {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public UserStory find(Integer id) {
+		return entityManager.find(UserStory.class, id);
+	}
+	
+	public Person findPerson(String firstName) {
+		return entityManager.find(Person.class, firstName);	
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<UserStory> getUserStoryList() {
+		return entityManager.createQuery("select u from UserStory u").getResultList();
+	}
+	
+	public UserStory getPublicUserStory(WorkBoard workboard) {	
+		UserStory userstory = null ;
+		Query query = entityManager.createQuery("select u from UserStory u where u.access = :access and u.workboard = :workboard") ;
+		query.setParameter("access", "public") ;
+		query.setParameter("workboard", workboard) ;
+		
+		try{
+			userstory = (UserStory) query.getSingleResult() ;	
+		} catch (NoResultException ne){
+			return userstory ;			
+		}
+		
+		return userstory ; 
+	}
+		
+	@Transactional
+	public UserStory save(UserStory userstory) {
+		if (userstory.getUserstoryid() == 0) {
+			entityManager.persist(userstory);
+			return userstory;
+		} else {
+			return entityManager.merge(userstory) ;
+		}		
+	}	
+	
+	@Transactional
+	public void removeUserStory(Integer id, UserStory userstory) {
+		int localid = id ;
+		Query query = entityManager.createQuery("delete from DataElement d where d.userstory = :userstory") ;
+		query.setParameter("userstory", userstory).executeUpdate();
+		query = entityManager.createQuery("delete from UserStory u where u.userstoryid = :ID") ;
+		query.setParameter("ID", localid).executeUpdate();
+	}
+	
+}
