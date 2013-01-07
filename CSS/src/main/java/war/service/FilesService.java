8ce151d612f033b244f92987a8f42907b24e3f74
Venki Balasubramanian <venki.balasubramanian@rmit.edu.au>
2013-01-07 04:58:45 +0000

    Updated the file service for the User story
    git-svn-id: file:///Users/ravisreenivas/GitConversions/local-svn/SEAPORTS/trunk@560 e6edf6fb-f266-4316-afb4-e53d95876a76

--- CSS/src/main/java/war/service/FilesService.java
+++ CSS/src/main/java/war/service/FilesService.java
@@ -1,18 +1,11 @@
 package war.service;
 
 import war.model.* ;
-//import war.dao.*;
 
 import java.util.List ;
 
 public class FilesService {
 	private List<Files> files ;
-/*	
- * 
- * 
- * private String splitvar = "\\." ;
-	private String [] tokens;
-	private FilesDao filesdao ;*/
 
 	public List<Files> getFiles() {	
 		return files;
@@ -21,22 +14,5 @@ public class FilesService {
 	public void setFiles(List<Files> files) { 
 		this.files = files ;	
 	}
-
-/*	
- * The below functionality is placed in the work board controller.
- * 
- * public void convertFile(byte [] bytes, String filename, WorkBoard workboard){
-		
-		tokens = filename.split(splitvar) ;
-		System.out.println("Inside File Service " + tokens[0] + "  " + tokens[1]);
-		Files file = new Files() ;
-		filesdao = new FilesDao();
-		file.setFile(bytes) ;
-		file.setFilename(tokens[0]) ;
-		file.setType(tokens[1]) ;
-		file.setWorkboard(workboard) ;	
-		System.out.println("Inside File Service " + workboard);
-		filesdao.save(file) ;
-	}*/
 
 }
